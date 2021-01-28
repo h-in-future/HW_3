1e242daffd3a8b9b374856a7942d4a5dc6a14148
shelemetev <43704293+shelemetev@users.noreply.github.com>
2021-01-28 15:57:15 +0300

    GRRERRERFDFS

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,5 +20,27 @@ public class Main {
 	    {
             System.out.println("Осень");
         }
+	int[] array={1,2,4,21,342,1,23,42,12,34};
+	for(int i=9;i>=0;i--)
+	{
+	    System.out.print(array[i]+" ");
+    }
+	System.out.println();
+	int[][] squareArray= new int[2][2];
+	squareArray[0][0]=12;
+	squareArray[0][1]=16;
+	squareArray[1][0]=22;
+	squareArray[1][1]=93;
+        int a=0;
+        int i=0;
+        for(a=0;a<=1;a++){
+        for(i=0;i<=1;i++){
+        System.out.print(squareArray[i][a]+" " +"["+i+"]"+"["+a+"] ");
+        i++;
+            System.out.println(squareArray[i][a]+" " +"["+i+"]"+"["+a+"]");
+
+        }
+          if(a>1){break;}
+    }
     }
 }
